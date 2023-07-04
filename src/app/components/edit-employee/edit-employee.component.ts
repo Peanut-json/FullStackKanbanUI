f203b32564994ad71b38353dce6c4f70134ac137
Peanut-json <retroownerxd@gmail.com>
2023-07-04 14:16:27 +0100

    implimentation of Edit functionality

--- src/app/components/edit-employee/edit-employee.component.ts
+++ src/app/components/edit-employee/edit-employee.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Route, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Employee } from 'src/app/Models/employee.model';
 import { EmployeesService } from 'src/app/Services/employees.service';
 
 @Component({
@@ -9,7 +11,14 @@ import { EmployeesService } from 'src/app/Services/employees.service';
 })
 export class EditEmployeeComponent implements OnInit {
 
+employeeDetails: Employee = {
 
+  id: '',
+  name: '',
+  email: '',
+  phone: 0
+
+}
 
 
 constructor(private route:ActivatedRoute , private employeeService: EmployeesService , private router: Router) {}
@@ -20,14 +29,25 @@ constructor(private route:ActivatedRoute , private employeeService: EmployeesSer
       next: (params) => {
         const id = params.get('id');
 
-        if (id) {
-          
+        if (id) {  //* this will call the API 
+
+          this.employeeService.getEmployee(id)
+          .subscribe({
+            next: (responce) => {
+              this.employeeDetails = responce;
+            }
+          })
         }
       }
     })
   }
 
-
-
-
+  updateEmployee(){
+    this.employeeService.updateEmpoloyee(this.employeeDetails.id ,this.employeeDetails)
+    .subscribe({
+      next: (response) => {
+        this.router.navigate(['employees'])
+      }
+    })
+  }
 }
